05e07a1435216d252c6a993daccd929ea4f68236
Simon Schoultz <simon.schoultz@cacevo.com>
2023-06-05 14:11:10 +0200

    trying test with faktory server as a service instead of testcontainers

--- Tests/FaktoryClientTests.cs
+++ Tests/FaktoryClientTests.cs
@@ -15,13 +15,34 @@ public class FaktoryClientTests
     }
 
     [Fact]
-    public async Task HappyFlow()
+    public async Task HappyFlow_Using_StandAlone_FaktoryServer()
     {
-        var dockerEndpoint = Environment.GetEnvironmentVariable("DOCKER_HOST") ?? "unix:/var/run/docker.sock";
+        await using var faktoryClient = new FaktoryClient("127.0.0.1", 7419);
+        await faktoryClient.ConnectAsync();
+        var wid = Guid.NewGuid().ToString();
+        await faktoryClient.HelloAsync("testworker", 2, wid);
+        await faktoryClient.HeartbeatAsync(wid);
         
+        //Push a job
+        var job = new FaktoryClient.Job(Guid.NewGuid().ToString(),"default", "SendEmail", new []{"test"});
+        await faktoryClient.PushJobAsync(job);
+        
+        //Fetch the job
+        var fetchedJob = await faktoryClient.FetchJobAsync(job.Queue);
+        fetchedJob.Should().NotBeNull();
+        fetchedJob!.JobId.Should().Be(job.JobId);
+        fetchedJob.Arguments.First().Should().Be("test");
+        fetchedJob.JobType.Should().Be("SendEmail");
+        
+        //Acknowledge the job is done
+        await faktoryClient.AckJobAsync(fetchedJob!.JobId);
+    }
+    
+    [Fact (Skip = "Can't get it to work in github actions")]
+    public async Task HappyFlow_Using_TestContainers()
+    {
         // Start a Faktory Job Server container locally for this test
         var container = new ContainerBuilder()
-            .WithDockerEndpoint(dockerEndpoint)
             .WithImage("contribsys/faktory:latest")
             .WithPortBinding(7419, true)
             .WithExposedPort(7419)
@@ -34,6 +55,7 @@ public class FaktoryClientTests
         
         //Connect to server
         await using var faktoryClient = new FaktoryClient(container.Hostname, container.GetMappedPublicPort(7419));
+        
         await faktoryClient.ConnectAsync();
         var wid = Guid.NewGuid().ToString();
         await faktoryClient.HelloAsync("testworker", 2, wid);
